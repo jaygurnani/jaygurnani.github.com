c98fea301d7bb8f87c3af133cf403a0122e3c048
Jay Gurnani <jay.gurnani@gmail.com>
2015-06-24 21:10:11 +1000

    Papa's website to check

---
+++ raychin/assets/js/main.js
@@ -0,0 +1,29 @@
+function initialize() {
+  var mapCanvas = document.getElementById('map-canvas');
+  var myLatLong = new google.maps.LatLng(25.065503, 121.525857);
+  var mapOptions = {
+    center: myLatLong,
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions)
+  var marker = new google.maps.Marker({
+    position: myLatLong,
+    map: map,
+    title: 'RayChin International Co. Ltd'
+  });
+
+}
+
+
+$(document).ready(function() {
+
+  $("#owl-demo").owlCarousel({
+    items : 4,
+    lazyLoad : true,
+    navigation : true
+  });
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+});
